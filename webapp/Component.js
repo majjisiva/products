c69adbc467660d86813766214073770f7e3971be
Siva Krishna Majji <mskrishna.msk@gmail.com>
2025-12-06 14:10:39 +0000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.sk.products.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
